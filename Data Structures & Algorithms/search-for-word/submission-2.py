69a94ab28d55546da0d2bbea3ef0e88b2ba1a4a8
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-06-16 14:04:55 -0700

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        visited = set()
+        def recurse(row, col, pos):
+            # boundaries
+            if 0>row or row>=len(board) or 0>col or col>=len(board[0]):
+                return False
+            # visited
+            if (row, col) in visited:
+                # print('vis: ', visited)
+                return False
+            #no match
+            if board[row][col] != word[pos]:
+                return False
+            visited.add((row, col))
+            # print('looking for: ', word[pos], ', row: ', row, ', col: ', col, ', pos: ', pos)
+            # print('-    Found: ', word[pos])
+            pos += 1
+            if pos == len(word):
+                return True
+            if recurse(row+1, col, pos) or recurse(row-1, col, pos) or recurse(row, col+1, pos) or recurse(row, col-1, pos):
+                return True
+            
+            visited.remove((row, col))
+            return False
+
+
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if recurse(i,j,0) == True:
+                    return True       
+
+        return False
+
+        
