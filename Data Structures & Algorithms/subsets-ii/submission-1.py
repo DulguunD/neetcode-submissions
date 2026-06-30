fde56cd900518bcf64efb7ae3637854c5154101a
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-06-30 14:31:46 -0700

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        seen = set()
+        nums.sort()
+        def recurse(index, path):
+            if path in result:
+                return
+            result.append(path[:])
+            for i in range(index, len(nums)):
+                if i not in seen:
+                    seen.add(i)
+                    path.append(nums[i])
+                    # print(f"Index: {i}, Value: {nums[i]}, Path: {path}")
+                    recurse(i, path)
+                    seen.remove(i)
+                    path.pop()
+        recurse(0, [])
+        return result
+        
