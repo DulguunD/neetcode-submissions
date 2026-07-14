76924d4ea7fe1dc326c6317f4473365c71f78784
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-14 13:53:56 -0700

    Add: longest-repeating-substring-with-replacement - submission-17

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-17.py
@@ -0,0 +1,26 @@
+# from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        left = 0
+        ans = 0
+        max_freq = 0
+        for right in range(len(s)):
+            # print(f"\tleft: {left}, right: {right}, max_freq: {max_freq}, ans: {ans}")
+            count[s[right]] += 1
+            max_freq = max(max_freq, count[s[right]])
+
+            # window_size = right-left+1
+            # replacements_needed = window_size-max_freq
+
+            # slide window to the left (shrink the window) - window is not valid
+            while (right - left + 1) - max_freq > k:
+                # print(f"left: {left}, left char: {s[left]} , count: {count}")
+                count[s[left]] -= 1
+                left += 1
+            ans = max(ans, right-left+1)
+        return ans
+
+      
+      
