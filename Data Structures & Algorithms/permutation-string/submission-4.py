688257e3aebfeede0f09b96d0bc9e0eaebaef399
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-17 20:37:42 -0700

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left = 0
+        found = defaultdict(int)
+        # found = {}
+        need = Counter(s1)
+        # window = defaultdict(int)
+        formed = 0
+        required = len(s1)
+        for right, char in enumerate(s2):
+            found[char] += 1
+
+            while left < right and (s2[left] not in s1 or found[s2[left]] > need[s2[left]]):
+                found[s2[left]] -= 1
+                left += 1
+
+            window = s2[left:right+1]
+            if right-left+1==required:
+                same = True
+                # print(f"window:  {window}")
+                for key in need.keys():
+                    if need[key] != found[key]:
+                        same = False
+                        break
+                      
+                if same:
+                    return True
+
+
+            # print(f"left: {left}, right: {right}, window: {window}, found: {found}")
+        return False
+
+
+        
