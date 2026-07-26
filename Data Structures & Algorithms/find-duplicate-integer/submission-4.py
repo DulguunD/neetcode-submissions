67a4e1cadc1aed1216c0683e7568c1d942943224
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-26 00:27:10 -0700

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+   
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        index = 0
+
+        # find intersection inside the cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow==fast:
+                break
+        
+        finder = nums[0]
+
+        while finder != slow:
+            finder = nums[finder]
+            slow = nums[slow]
+            
+        return finder
