e0b74a7fa779a9018e11635fd29006b7058e2abe
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-31 23:28:28 -0700

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        pending = []
+        result = [0]*len(temperatures)
+        for index, temp in enumerate(temperatures):
+            while pending and pending[-1][0] < temp:
+                result[pending[-1][1]] += (index - pending[-1][1])
+                pending.pop()
+            pending.append((temp, index))
+        return result        
