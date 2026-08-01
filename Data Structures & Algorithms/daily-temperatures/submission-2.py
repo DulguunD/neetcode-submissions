4e565f1f7b58221dc54ac91c40fee265a9a683e3
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-31 23:19:36 -0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        pending = []
+        result = [0]*len(temperatures)
+        # result = []
+        for index, temp in enumerate(temperatures):
+            print(f"temp: {temp}, index: {index}, pending: {pending}")
+            while pending and pending[-1][0] < temp:
+                result[pending[-1][1]] += (index - pending[-1][1])
+                pending.pop()
+            pending.append((temp, index))
+            # if pending and temp > pending[-1][0]:
+            #     # print(f"")
+            #     print(f"current index: {index}, temperature: {temp}, pending: {pending[-1][1]}, pending temp: {pending[-1][0]}")
+            #     # result[pending[-1][1]] += (index - pending[-1][1])
+            #     # result[pending[-1][1]] += (pending[-1][1])
+            #     # result.append((index - pending[-1][1]))
+            #     # pending.pop()
+            #     while pending and pending[-1][0] < temp:
+            #         result[pending[-1][1]] += (index - pending[-1][1])
+            #         pending.pop()
+            #     pending.append((temp, index))
+            # else:
+            #     pending.append((temp, index))
+
+        return result        
