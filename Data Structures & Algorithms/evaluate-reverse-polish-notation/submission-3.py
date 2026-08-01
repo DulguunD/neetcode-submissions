84218d1493ac8adfaaec61c7669b0198e5fbcb14
Dulguun <72278859+DulguunD@users.noreply.github.com>
2026-07-31 20:27:29 -0700

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        result = 0
+        # pending = []
+        nums = []
+        operations = []
+        operators = "+-*/"
+        # pending
+        for token in tokens:
+            if token in operators:
+                print(f"Operation: {token}")
+                # operations.append(token)
+
+                num1 = int(nums.pop())
+                num2 = int(nums.pop())
+                res = 0
+                if token == "+":
+                    res = num1+num2
+                elif token == "-":
+                    res= num2-num1
+                elif token == "*":
+                    res = num1*num2
+                elif token == "/":
+                    res = num2/num1
+                print(f"num1: {num1}, num2: {num2}, result = {res}")
+                nums.append(res)
+            else:
+                # integer, num
+                nums.append(token)
+            # pending.append(token)
+        print(f"pending: {nums}, Operations: {operations}")
+        return int(nums[0])
+        
